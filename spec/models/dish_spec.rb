0602aa503ce6e545a33ea86d5368473b758a268e
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-20 15:48:52 -0700

    Add user story 2 total calories test to dish spec

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -11,5 +11,21 @@ RSpec.describe Dish, type: :model do
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
 
+  before :each do
+    @chef = Chef.create!(name: "Taryn")
+
+    @dish_1 = Dish.create!(name: "Mac and Cheese", description: "Basic comfort food", chef_id: @chef.id)
+
+    @mac = Ingredient.create!(name: "Macaroni", calories: 100)
+    @cheese = Ingredient.create!(name: "Cheese", calories: 200)
+
+    @dish_1_ingredient_1 = DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @mac.id)
+    @dish_1_ingredient_2 = DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @cheese.id)
+  end
+
+  it "can calculate total calories of a dish" do
+    expect(@dish_1.total_calories).to eq(300)
+  end
+
   
 end
